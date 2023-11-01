4e34b71168e328ae14db831911c1dbfcb51f1f5c
rgi-group <74689120+rgi-group@users.noreply.github.com>
2023-11-01 18:52:28 -0500

    Update Unusal Geo Location.sql
    removed pi

--- AWS Athena Searches/Okta/Unusal Geo Location.sql
+++ AWS Athena Searches/Okta/Unusal Geo Location.sql
@@ -1,6 +1,6 @@
 # Unusal Geo Location 
 SELECT detail.client.geographicalcontext.country AS country, COUNT(*) AS count
-FROM "your_database"."postman_s3_okta_audit_logs"
+FROM  "your_database"."your_table"
 GROUP BY detail.client.geographicalcontext.country
 HAVING COUNT(*) < 5;
 
