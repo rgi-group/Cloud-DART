66f21524911ca97092dfccc4f878a6e3c83c4e8d
rgi-group <74689120+rgi-group@users.noreply.github.com>
2023-11-01 18:49:58 -0500

    Update Detect Suspicious IP Addresses.sql
    removed PI

--- AWS Athena Searches/Okta/Detect Suspicious IP Addresses.sql
+++ AWS Athena Searches/Okta/Detect Suspicious IP Addresses.sql
@@ -1,6 +1,6 @@
 #Detect Suspicious IP Addresses
 SELECT detail.client.ipaddress AS suspicious_ip, COUNT(*) AS count
-FROM "your_database"."postman_s3_okta_audit_logs"
+FROM "your_database"."you_table"
 WHERE detail.outcome.result = 'FAILURE'
 GROUP BY detail.client.ipaddress
 HAVING COUNT(*) > 5;
