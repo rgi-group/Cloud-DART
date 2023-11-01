fa004789b1ad2af458454ae320a84fcf2a125f90
rgi-group <74689120+rgi-group@users.noreply.github.com>
2023-11-01 18:50:29 -0500

    Update Detect Unusal User Agents.sql
    removed pi

--- AWS Athena Searches/Okta/Detect Unusal User Agents.sql
+++ AWS Athena Searches/Okta/Detect Unusal User Agents.sql
@@ -1,5 +1,5 @@
 # Detect Unusal User Agents
 SELECT detail.client.useragent.rawuseragent AS user_agent, COUNT(*) AS count
-FROM "your_database"."postman_s3_okta_audit_logs"
+FROM ""your_database"."you_table""
 GROUP BY detail.client.useragent.rawuseragent
 HAVING COUNT(*) < 3;
